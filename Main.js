42ac7046d1f19bfe030ba8f76353cb4e9e1c66ec
GSrisanth <srisanthchowdary@gmail.com>
2025-12-18 21:12:26 +0530

    Initial commit: Full Stack Development Journey

---
+++ Main.js
@@ -0,0 +1,16 @@
+console.log("Hello, World!");
+console.log(42);
+console.log(true);
+
+console.log(2 + 2); // Outputs: 4
+console.log("2 + 2"); // Outputs: 2 + 2
+console.log("Hello, " + "World!"); // Outputs: Hello, World!
+console.log("2" + "2"); // Outputs: 22, because + operation applied on strings concatenates them
+console.log(2 + 2 > 5); // Outputs: false, because 2 + 2 is not greater than 5.
+
+//Comparision Operators
+
+console.log(5 < 6);   // true
+console.log(5 > 6);   // false
+console.log(5 <= 5);  // true
+console.log(5 >= 10); // false
